a107aa61d19e907bb6b85063741750607a66619e
Brentwood Reid <bwoodreid@gmail.com>
2020-03-10 12:50:19 -0700

    Add incident type and format incident name

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,4 +1,8 @@
 class Incident < ApplicationRecord
   has_many :plans, dependent: :destroy
   has_many :resources, dependent: :destroy
+
+  def display_incident_name
+    "#{self.name}  –  #{self.incident_type} #{self.number}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190815183336) do
+ActiveRecord::Schema.define(version: 20200310164906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20190815183336) do
     t.datetime "updated_at", null: false
     t.integer "size"
     t.string "financial_code"
+    t.string "incident_type"
+    t.string "complexity"
   end
 
   create_table "objectives", force: :cascade do |t|
