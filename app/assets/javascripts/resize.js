4b131127bf4aa95fea426676f98b0c6459514458
Brentwood Reid <bwoodreid@gmail.com>
2022-08-06 11:04:15 -0700

    Resize blocks

--- app/assets/javascripts/resize.js
+++ app/assets/javascripts/resize.js
@@ -4,9 +4,9 @@ $(document).on("turbolinks:load", function() {
     $('.resizer').toggle();
     $('resizer').toggle('.bordered')
   });
-  
+
   /*Make resizable div by Hung Nguyen*/
-  function makeResizableDiv(div) {
+  function makeResizableImage(div) {
     const element = document.querySelector(div + ' .resizers');
     const resizers = document.querySelectorAll(div + ' .resizer')
     const minimum_size = 20;
@@ -21,7 +21,7 @@ $(document).on("turbolinks:load", function() {
       currentResizer.addEventListener('mousedown', function(e) {
         e.preventDefault()
         original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
-        // original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+        original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
         original_x = element.getBoundingClientRect().left;
         original_y = element.getBoundingClientRect().top;
         original_mouse_x = e.pageX;
@@ -83,6 +83,53 @@ $(document).on("turbolinks:load", function() {
     }
   }
 
-  makeResizableDiv('.resizable')
+  makeResizableImage('.resizable')
+
+  $('.resize-padding').hover(function(){
+    $('.resize-padding').removeClass('selected-resizer')
+    $(this).addClass('selected-resizer');
+  });
+
+   /*Make resizable div by Hung Nguyen*/
+  function makeResizableBlock(div) {
+    // const element = document.querySelector(div + ' .bottom-padding');
+    const resizers = document.querySelectorAll(div + ' .resize-padding')
+    const minimum_size = 0;
+    // let original_width = 0;
+    let original_height = 0;
+    // let original_x = 0;
+    let original_y = 0;
+    // let original_mouse_x = 0;
+    let original_mouse_y = 0;
+    for (let i = 0;i < resizers.length; i++) {
+      const currentResizer = resizers[i];
+      const parent = currentResizer.closest('.bottom-padding')
+      currentResizer.addEventListener('mousedown', function(e) {
+        e.preventDefault()
+        // original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+        original_height = parseFloat(getComputedStyle(parent, null).getPropertyValue('height').replace('px', ''));
+        // original_x = parent.getBoundingClientRect().left;
+        original_y = parent.getBoundingClientRect().top;
+        // original_mouse_x = e.pageX;
+        original_mouse_y = e.pageY;
+        window.addEventListener('mousemove', resize_block)
+        window.addEventListener('mouseup', stopResize)
+      })
+      
+      function resize_block(e) {
+          const height = original_height + (e.pageY - original_mouse_y)
+
+          if (height > minimum_size) {
+            parent.style.height = height + 'px'
+          }
+      }
+      
+      function stopResize() {
+        window.removeEventListener('mousemove', resize_block)
+      }
+    }
+  }
+
+  makeResizableBlock('.bottom-padding')
 
 });
