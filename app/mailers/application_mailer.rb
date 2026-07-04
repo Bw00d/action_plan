ce4241c950caed4c69f8967ddc57c60aa67d6752
bw00d <bwoodreid@gmail.com>
2026-07-04 09:54:02 -0800

    Configurable mailer From address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,8 @@
 # :nocov:
 class ApplicationMailer < ActionMailer::Base
-  default from: 'bwoodreid@gmail.com'
+  # From address is env-configurable so the sender matches the Mailgun
+  # domain in each deployed environment (sandbox or verified).
+  default from: ENV.fetch('MAIL_FROM', 'Action Plan <bwoodreid@gmail.com>')
   layout 'mailer'
 end
 # :nocov:
